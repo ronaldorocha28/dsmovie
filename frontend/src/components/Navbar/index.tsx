3377bd20196bb89a25c5aa1d7b861f3145e7d69a
Ronaldo Rocha <ronaldo.rocha28@hotmail.com>
2022-01-13 16:35:53 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar() {
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
                     <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
-                        <div>
+                        <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/devsuperior</p>
                         </div>
