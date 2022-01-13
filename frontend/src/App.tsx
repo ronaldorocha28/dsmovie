e8492de04464e0c710b3f18163dcbcda79b43d99
Ronaldo Rocha <ronaldo.rocha28@hotmail.com>
2022-01-13 00:21:33 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import Navbar from "./components/Navbar";
 function App() {
-  return (
-   <h1>Página principal</h1>
-  );
+  
+    return(
+      <Navbar />
+    );
 }
 
 export default App;
